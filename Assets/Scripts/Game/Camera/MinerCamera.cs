ed79b6fbf28c5cffea3f37968713ee840cc7b009
corrado <corradolongoni@yahoo.com>
2019-04-28 11:15:12 +0200

    skeleton for camera

---
+++ Assets/Scripts/Game/Camera/MinerCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinerCamera : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0, 0, -126);
+        Debug.Log("player " + player.transform.position.x);
+    }
+}
