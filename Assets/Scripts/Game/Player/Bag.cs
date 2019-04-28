8c36ede78afd37a9a3c68aa3b0c724b218e04197
unknown <corradolongoni@gmail.com>
2019-04-28 16:52:03 +0200

    scripte bag

---
+++ Assets/Scripts/Game/Player/Bag.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bag : MonoBehaviour
+{
+    public int size = 3;
+    private List<string> gems = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool AddGem(string name)
+    {
+        if (gems.Count >= size) return false;
+        gems.Add(name);
+        return true;
+    }
+
+    public void EmptyBag()
+    {
+        gems.RemoveAll(gem =>
+        {
+            return true;
+        });
+    }
+}
