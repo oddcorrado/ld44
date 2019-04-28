a8388d76cdd2a4008072619f0caff4abb911dc04
unknown <corradolongoni@gmail.com>
2019-04-28 12:49:20 +0200

    Spike V.0

---
+++ Assets/Scripts/Game/Hazards/Spike.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject go = collision.collider.gameObject;
+        LifeManager lm = go.GetComponent<LifeManager>();
+
+        if (lm == null) return;
+        lm.Life = lm.Life - 15;
+ 
+    }
+}
