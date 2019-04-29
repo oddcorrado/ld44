53a38f7c7152eece473d91bcfcc5cea916bdee73
corrado <corradolongoni@yahoo.com>
2019-04-29 22:43:29 +0200

    last minute fixes

---
+++ Assets/Scripts/Game/Player/Transformation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transformation : MonoBehaviour
+{
+    public Animator animator;
+    public ParticleSystem particleSystem;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<InputRouter>() == null) return;
+
+        Debug.Log("transform");
+        collision.gameObject.SetActive(false);
+        animator.SetBool("go", true);
+        particleSystem.Stop();
+    }
+}
